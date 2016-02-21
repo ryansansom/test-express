67f1830d755b2c7ad7b7a34ca32d97d20efacb39
ryman69 <ryan.sansom@uk.tesco.com>
2016-02-21 16:39:45 +0000

    Added initial files

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+import routes from './routes/index';
+import users from './routes/users';
+
+import {notFound, devError, prodError} from './routes/error-handler';
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon(path.join(__dirname, 'public/favicon', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(notFound);
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(devError);
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(prodError);
+
+
+module.exports = app;
