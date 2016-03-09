218d3fd0c06fece9ef6f94b3a5e0577022828842
Ryan Sansom <ryman69@hotmail.co.uk>
2016-03-09 15:06:15 +0000

    Added JS expandable menu

--- react/components/main-layout/index.js
+++ react/components/main-layout/index.js
@@ -1,24 +1,40 @@
+import classnames from 'classnames';
 import React from 'react';
 
 if (process.env.CLIENT_RENDER) {
   require('./small.less');
 }
 
-// Will become main layout
 export default class MainLayout extends React.Component {
- render() {
-   return (
-     <div>
-       <div className='header'>
-         <span className='icon-brand-logo' />
-         <a
-           className='header--menu'
-           onClick={}>
-           {'Menu'}
-         </a>
-       </div>
-       {this.props.children}
-     </div>
-   );
- }
+  constructor() {
+    super();
+    this.state = {menuOpen: false};
+    this.doSomethingOnClick = this.doSomethingOnClick.bind(this);
+  }
+  doSomethingOnClick(evt){
+    evt.preventDefault();
+    return this.setState({menuOpen: !this.state.menuOpen});
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='header'>
+          <span className='icon-brand-logo' />
+          <a
+            className='header--menu'
+            onClick={this.doSomethingOnClick}>
+            {'Menu'}
+          </a>
+        </div>
+        <div className={classnames({
+          'menu--container': true,
+          'is-open': this.state.menuOpen
+        })}>
+
+        </div>
+        {this.props.children}
+      </div>
+    );
+  }
 }
